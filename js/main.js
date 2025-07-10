d1b267d470c4f5c5916332842f9a20789c476b19
Serhii269 <podgorodeckojsega@gmail.com>
2025-07-10 18:34:23 +0200

    created section 3

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-const btn1 = document.querySelector(".btn-primary");
-const btn2 = document.querySelector(".btn-secondary");
+// Первая секция (Hero)
+const heroBtn1 = document.querySelector(".hero-section .btn-primary");
+const heroBtn2 = document.querySelector(".hero-section .btn-secondary");
 
-btn2.addEventListener("mouseenter", () => {
-  btn1.classList.remove("active");
-  btn2.classList.add("active");
-});
+if (heroBtn1 && heroBtn2) {
+  heroBtn2.addEventListener("mouseenter", () => {
+    heroBtn1.classList.remove("active");
+    heroBtn2.classList.add("active");
+  });
 
-btn2.addEventListener("mouseleave", () => {
-  btn1.classList.add("active");
-  btn2.classList.remove("active");
-});
+  heroBtn2.addEventListener("mouseleave", () => {
+    heroBtn1.classList.add("active");
+    heroBtn2.classList.remove("active");
+  });
+}
+
+// Вторая секция (Sell NFT)
+const sellBtn1 = document.querySelector(".sell-nft-section .btn-primary");
+const sellBtn2 = document.querySelector(".sell-nft-section .btn-secondary");
+
+if (sellBtn1 && sellBtn2) {
+  sellBtn2.addEventListener("mouseenter", () => {
+    sellBtn1.classList.remove("active");
+    sellBtn2.classList.add("active");
+  });
+
+  sellBtn2.addEventListener("mouseleave", () => {
+    sellBtn1.classList.add("active");
+    sellBtn2.classList.remove("active");
+  });
+}
